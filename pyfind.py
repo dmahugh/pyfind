e3b9bdbdcff1b8965ad6da211cd8b097cc2a8a91
Doug Mahugh <dmahugh@microsoft.com>
2019-05-04 12:36:14 +0100

    refactoring/simplification

--- pyfind.py
+++ pyfind.py
@@ -12,6 +12,59 @@ import click
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
+class _settings:
+    """Provides a namespace used for global settings. Used only for totals.
+    """
+
+    folders_searched = 0
+    files_searched = 0
+    lines_searched = 0
+    bytes_searched = 0
+
+
+class MatchPrinter:
+    """Prints matches as they're found.
+    """
+
+    def __init__(self):
+        self.folder = ""
+        self.filename = ""
+
+    def display(self, match):
+        """Display a search hit.
+
+        1st parameter = dictionary of folder, filename, location, linetext
+        """
+        folder = match["folder"]
+        filename = match["filename"]
+        location = match["location"]
+        linetext = match["linetext"]
+
+        if folder != self.folder:
+            click.echo(click.style(f"  folder: {folder}", fg="bright_green"))
+            self.folder = folder
+            # handle special case of the same-named file being a search hit
+            # in two different folders, by resetting self.filename to an impossible
+            # value to force display of the filename below even if it hasn't
+            # changed since the previous hit ...
+            self.filename = " / "
+
+        if filename != self.filename:
+            click.echo(f"          {filename}")
+            self.filename = filename
+
+        # print the matching line
+        loc_str = location.rjust(8) + ": "
+        toprint = linetext.strip()[:100]
+        try:
+            click.echo(click.style(loc_str + toprint, fg="cyan"))
+        except UnicodeEncodeError:
+            toprint = str(linetext.encode("utf8"))
+            if len(toprint) > 67:
+                toprint = toprint[:64] + "..."
+            click.echo(click.style(loc_str + toprint, fg="cyan"))
+
+
 @click.argument("startdir", default="*projects", metavar="<startdir>")
 @click.argument("searchfor", metavar="searchfor")
 @click.command(context_settings=CONTEXT_SETTINGS, options_metavar="<options>")
@@ -64,16 +117,6 @@ def cli(searchfor, startdir, filetypes, subfolders):
     )
 
 
-class _settings:
-    """Provides a namespace used for global settings. Used only for totals.
-    """
-
-    folders_searched = 0
-    files_searched = 0
-    lines_searched = 0
-    bytes_searched = 0
-
-
 def get_matches(*, searchfor="", startdir=Path.cwd(), filetypes=None, subfolders=False):
     """Searches files and returns a list of matches.
 
@@ -98,29 +141,7 @@ def get_matches(*, searchfor="", startdir=Path.cwd(), filetypes=None, subfolders
     output = MatchPrinter()
 
     if startdir.lower().startswith("*project"):
-        # this is a special case, which we handle here and return
-        pyfind_folder = Path(__file__).resolve().parent
-        projects_file = Path.joinpath(pyfind_folder, "projects.txt")
-        if not projects_file.is_file():
-            click.echo(click.style(f"FILE NOT FOUND: {projects_file}", fg="red"))
-            return
-        matchlist = []
-        for line in projects_file.read_text().splitlines():
-            folder = Path(line.strip())
-            if not folder.is_dir():
-                click.echo(click.style(f"FOLDER NOT FOUND: {folder}", fg="red"))
-                continue
-            _settings.folders_searched += 1
-            for filename in glob(str(folder.joinpath("*.*"))):
-                file_to_search = Path(filename)
-                if file_to_search.suffix not in filetypes:
-                    continue
-                _settings.files_searched += 1
-                for match in search_file(file_to_search, searchfor, folder):
-                    matchlist.append(match)
-                    output.display(match)
-
-        print_summary(matchlist)
+        search_projects(searchfor, filetypes, output)
         return
 
     if startdir.lower().startswith("*package"):
@@ -148,6 +169,7 @@ def get_matches(*, searchfor="", startdir=Path.cwd(), filetypes=None, subfolders
                 for match in search_file(file_to_search, searchfor, current_folder):
                     matchlist.append(match)
                     output.display(match)
+
     print_summary(matchlist)
 
 
@@ -236,44 +258,37 @@ def search_file(file, searchfor, folder):
     return matches
 
 
-class MatchPrinter:
-    """Prints matches as they're found.
+def search_projects(search_for, file_types, match_printer):
+    """Searches a list of project folders and displays matches found.
+
+    Args:
+        search_for: The text to search for.
+        file_types: The list of file types to search (e.g., [".py", ".ipynb"]).
+        match_printer: A MatchPrinter instance to use for printing the
+            found matches to the console.
+
+    Returns:
+        None. Matches are printed to the console.
     """
+    pyfind_folder = Path(__file__).resolve().parent
+    projects_file = Path.joinpath(pyfind_folder, "projects.txt")
+    if not projects_file.is_file():
+        click.echo(click.style(f"FILE NOT FOUND: {projects_file}", fg="red"))
+        return
+    matchlist = []
+    for line in projects_file.read_text().splitlines():
+        folder = Path(line.strip())
+        if not folder.is_dir():
+            click.echo(click.style(f"FOLDER NOT FOUND: {folder}", fg="red"))
+            continue
+        _settings.folders_searched += 1
+        for filename in glob(str(folder.joinpath("*.*"))):
+            file_to_search = Path(filename)
+            if file_to_search.suffix not in file_types:
+                continue
+            _settings.files_searched += 1
+            for match in search_file(file_to_search, search_for, folder):
+                matchlist.append(match)
+                match_printer.display(match)
 
-    def __init__(self):
-        self.folder = ""
-        self.filename = ""
-
-    def display(self, match_tuple):
-        """Display a search hit.
-
-        1st parameter = dictionary of folder, filename, location, linetext
-        """
-        folder = match_tuple["folder"]
-        filename = match_tuple["filename"]
-        location = match_tuple["location"]
-        linetext = match_tuple["linetext"]
-
-        if folder != self.folder:
-            click.echo(click.style(f"  folder: {folder}", fg="bright_green"))
-            self.folder = folder
-            # handle special case of the same-named file being a search hit
-            # in two different folders, by resetting self.filename to an impossible
-            # value to force display of the filename below even if it hasn't
-            # changed since the previous hit ...
-            self.filename = " / "
-
-        if filename != self.filename:
-            click.echo(f"          {filename}")
-            self.filename = filename
-
-        # print the matching line
-        loc_str = location.rjust(8) + ": "
-        toprint = linetext.strip()[:100]
-        try:
-            click.echo(click.style(loc_str + toprint, fg="cyan"))
-        except UnicodeEncodeError:
-            toprint = str(linetext.encode("utf8"))
-            if len(toprint) > 67:
-                toprint = toprint[:64] + "..."
-            click.echo(click.style(loc_str + toprint, fg="cyan"))
+    print_summary(matchlist)
